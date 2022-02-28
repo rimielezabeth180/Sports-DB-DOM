1d0a38334fdf54faf5935956ff54fe71c94585fe
Rimi <rimielezabeth180@gmail.com>
2022-02-28 20:02:46 +0600

    Player Id faced

--- app.js
+++ app.js
@@ -4,7 +4,46 @@ const allPlayers = () => {
     // console.log(url);
     fetch(url)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        // .then((data) => console.log(data));
+        // To access first player
+        // .then((data) => console.log(data.player));
+        .then((data) => showPlayerDetails(data.player));
     // console.log(searchValue);
+};
+
+const showPlayerDetails = (players) => {
+    for (const player of players) {
+        // console.log(player);
+        // }
+
+
+        const parent = document.getElementById("player-container");
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card border p-5 mb-3">
+         <div class="pro-pic">
+          <img class="w-50" src="${player.strThumb}" alt="">
+         </div>
+         <h2>Name: ${player.strPlayer}</h2>
+         <h3>Country: ${player.strNationality}</h3>
+         <p></p>
+         <div class="all-button">
+           <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
+           <button class="btn btn-danger"> Delete</button>
+         </div>
+        </div>`;
+        parent.appendChild(div);
+        console.log(player);
+    }
+};
+
+const details = (id) => {
+    // console.log("OK", info);
+    // console.log(info);
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data.players[0]));
+
 };
 
